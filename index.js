4e64297803ba15286e7ddfd7781b654050f22fde
Florian <florian@jester.at>
2022-02-21 14:30:52 +0100

    Vue JS  und Pixi JS index eingebunden

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/infoseite/public/index.html'))
+});
 
-app.listen(3000, () => {
-    console.log('My REST API running on port 3000');
+app.get('/webgame/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/webgame/index.html'))
 })
+
+app.get('/infoseite/public/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/infoseite/public/index.html'))
+});
+
+let PORT = process.env.PORT || 80;
+
+app.listen(PORT, () => {
+    console.log(`My REST API running on port ${PORT}`);
+})
+
+// Zum starten --> npm run dev 
+// Für die GUI --> vue ui
