52c2ad26b7132df1d5a32c0e2900929420cc552f
Florian <florian@jester.at>
2022-03-01 19:58:26 +0100

    Erstes Probe Layout der Website

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const app = express();
 const path = require('path');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/infoseite/public/index.html'))
+    res.sendFile(path.join(__dirname, '/infoseite/index.html'))
 });
 
 app.get('/webgame/index.html', (req, res) => {
     res.sendFile(path.join(__dirname, '/webgame/index.html'))
 })
 
-app.get('/infoseite/public/index.html', (req, res) => {
-    res.sendFile(path.join(__dirname, '/infoseite/public/index.html'))
+app.get('/webgame/css/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname, '/webgame/css/style.css'))
+})
+
+app.get('/infoseite/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/infoseite/index.html'))
 });
 
 let PORT = process.env.PORT || 80;
@@ -21,4 +25,3 @@ app.listen(PORT, () => {
 })
 
 // Zum starten --> npm run dev 
-// Für die GUI --> vue ui
